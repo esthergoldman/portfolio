1b118c4d807e1fc0f5d63ab443f403383284c8e1
esthergoldman <77267248+esthergoldman@users.noreply.github.com>
2021-05-03 23:27:23 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,7 +138,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'esti.codes@gmail.com'
-EMAIL_HOST_PASSWORD = 'candida91'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Messages
